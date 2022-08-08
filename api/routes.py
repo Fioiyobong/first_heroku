749f00c22d1fe23142a00b42fe3fd072bc2e321b
Fioiyobong <umohfioiyo@gmail.com>
2022-08-08 15:23:08 +0100

    latest code

--- api/routes.py
+++ api/routes.py
@@ -3,6 +3,30 @@ from flask import Blueprint, jsonify
 
 api = Blueprint("api",__name__)
 
+data = [
+    {
+        "id" : 1,
+        "name" : "stone"
+
+    },
+    {
+        "id" : 2,
+        "name" : "fish"
+    },
+    {
+        "id" : 3,
+        "name" : "vegetables"
+    },
+    {
+        "id" : 4,
+        "name" : "beans"
+    },
+    {
+        "id" : 5,
+        "name" : "air-purifier"
+    }
+]
+
 
 @api.get("/blogs/")
 def blog_list():
@@ -10,10 +34,24 @@ def blog_list():
         {
             "name":"anies",
             "position":20
+        },
+
+        {
+            "name":"fioiyobong",
+            "age":2
+        },
+        {
+            "school":"aptech",
+            "address":"worldwide"
+        },
+        {
+            "gender":"female",
+            "blood-type":"AB+"
         }
     ])
 
 
 @api.get("/blogs/<int:id>")
 def blog_retrieve(id):
+    # fun function that filter a list of dictionary
     return "blog with id "+str(id)
